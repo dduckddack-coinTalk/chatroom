5b523d2d5303be8bf8b3efdf2161aaa0ee6e0075
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-05-04 16:10:54 +0900

    feat print helloWorld!

---
+++ src/test/java/com/example/kointalkchatroom/KoinTalkChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kointalkchatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoinTalkChatroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
